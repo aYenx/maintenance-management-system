d19264fafbfe00bbe9de71bf9291202d8cec3537
Yenx <yenx203@163.com>
2025-09-17 01:16:28 +0800

    检查是否配置高德地图api密钥

--- src/app/admin/locations/page.tsx
+++ src/app/admin/locations/page.tsx
@@ -70,6 +70,13 @@ export default function page() {
   const [lngLat, setLngLat] = useState<[number, number]>();
 
   function loadMap() {
+    const amapKey = process.env.NEXT_PUBLIC_AMAP_KEY;
+  
+  if (!amapKey) {
+    console.error('高德地图API密钥未配置');
+    toast.error('地图功能不可用，请联系管理员配置API密钥');
+    return;
+  }
     AMapLoader.load({
       key: process.env.NEXT_PUBLIC_AMAP_KEY,
       version: "2.0",
